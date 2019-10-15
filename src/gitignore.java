f5c6c158bd85abfa8977cf22c8540b7d8868b9e6
graffadamr <graff.adam.r@gmail.com>
2019-10-15 11:06:26 -0400

    first commit

---
+++ src/gitignore.java
@@ -0,0 +1,28 @@
+
+public class gitignore {
+
+	public static void main(String[] args) {
+		
+		# Eclipse
+		.classpath
+		.project
+		.settings/
+
+		# Intellij
+		.idea/
+		*.iml
+		*.iws
+
+		# Mac
+		.DS_Store
+
+		# Maven
+		log/
+		target/
+
+		*.class
+		/bin
+
+	}
+
+}
